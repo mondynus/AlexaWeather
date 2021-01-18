b541d15712fa001438a039651a948122173824ae
Marian Vajdecka <vajdecka@eset.sk>
2021-01-18 17:15:36 +0100

    Update weather-audio.php

--- weather-audio.php
+++ weather-audio.php
@@ -1,6 +1,6 @@
 <?php
-#https://developer.amazon.com/alexa/console/ask/
-#Flash Briefing skill
+//https://developer.amazon.com/alexa/console/ask/
+//Flash Briefing skill
 $date = new \DateTime('now');
 $datestring=$date->format('Y-m-d\TH:i:s\.\0\Z');
 header("Content-Type: application/json; charset=UTF-8");
